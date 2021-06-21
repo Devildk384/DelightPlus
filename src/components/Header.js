a57f0968121f157dbcba0ea1aeb0a4210e5bc517
Durgesh Kumar <durgeshkumar384@gmail.com>
2021-06-21 13:45:59 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <span>Delight Plus</span>
+      </div>
+      <span style={{ fontSize: "16px", paddingLeft: "20px" }}>
+        coming soon..
+      </span>
+    </header>
+  );
+};
+
+export default Header;
